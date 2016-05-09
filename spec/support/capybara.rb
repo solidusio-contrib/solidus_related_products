c9c63c2eafd64efe6e49fc6e1277dbcacf59e28a
John Hawthorn <john@stembolt.com>
2016-05-09 11:04:37 -0700

    Give capybara and poltergeist a generous wait time
    To avoid failures due to hiccups in the CI environment. Assets can take
    a long time to compile, needing a bump in poltergeist's timeout.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,13 +4,13 @@ require 'capybara/poltergeist'
 require 'capybara-screenshot/rspec'
 
 RSpec.configure do |config|
+  Capybara.register_driver(:poltergeist) do |app|
+    Capybara::Poltergeist::Driver.new app, js_errors: true, timeout: 90
+  end
   Capybara.javascript_driver = :poltergeist
+  Capybara.default_max_wait_time = 10
 
   Capybara.save_and_open_page_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
-
-  Capybara.register_driver(:poltergeist) do |app|
-    Capybara::Poltergeist::Driver.new app, js_errors: true, timeout: 30
-  end
 
   config.before(:each, :js) do
     if Capybara.javascript_driver == :selenium
