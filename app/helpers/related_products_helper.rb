1a6d611fcaf9cf933e641e8eed7b5c7ae76a1b35
Rainer Dema <rainer.dema@gmail.com>
2019-07-12 17:41:22 +0200

    Fix master link in releted variants product view
    For master variants, there isn't view dedicated to editing.
    If the variant is related to a master variant, return to the product edit page.

--- app/helpers/related_products_helper.rb
+++ app/helpers/related_products_helper.rb
@@ -5,6 +5,8 @@ module RelatedProductsHelper
     if object.instance_of? Spree::Product
       admin_product_path(object)
     elsif object.instance_of? Spree::Variant
+      return edit_admin_product_path(object.product) if object.is_master?
+
       edit_admin_product_variant_path(object.product, object)
     end
   end
